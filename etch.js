57a710e198b8c811ba858513077e4322769f3803
Austin <abharms@gmail.com>
2016-06-01 06:19:52 -0500

    finish project

--- etch.js
+++ etch.js
@@ -1,35 +1,45 @@
 //Insert a 16x16 grid of divs
 var $container = $("<div class='container'></div>");
-
 $("body").prepend($container);
 
 for(var i = 0; i < 16; i++){
 	for(var j = 0; j < 16; j++){
-		var $div = $("<div class='div1'></div>");
+		var $div = $("<div class='square'></div>");
 		$div.appendTo(".container");
 	}
 };
 
 //change colors on hover
-$(".div1").hover(function(){
-	$(this).addClass("newColor");
-})
+function hoverColor() {
+	$(".square").hover(function(){
+	$(this).addClass("hoverColor");
+  })
+};
+//invoke hover function
+hoverColor();
+
 
 
 //on reset
-	//prompt user for new grid size
 	function resetGrid() {
-		
+		//prompt user for new grid size
 		var newSquares = prompt("How many squares per side would you like?");
-		$(".div1").removeClass("newColor");
-		
+		//remove white hover color
+		$(".square").removeClass("newColor");
+		//remove all squares from container
+		$("div").removeClass("square");
 
+		//create new squares based on user input
 		for(var i = 0; i < newSquares; i++){
  			for(var j = 0; j < newSquares; j++){
-			var $newDiv = $("<div class='div2'></div>");
-			$("div").removeClass("div1");
+			var $newDiv = $("<div class='square'></div>");
 			$newDiv.appendTo(".container");
  	}
+ 		//change container dimensions based on amount of new squares user chose
+ 		var containerHeightWidth = (newSquares * 20) + (newSquares * 2);
+ 		$(".container").width(containerHeightWidth).height(containerHeightWidth);
+ 		//invoke hover function
+ 		hoverColor();
  };
 
 }
